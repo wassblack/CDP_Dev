ab047715a06d6e773ae77def8491300040e08deb
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-11-05 22:30:44 +0100

    tasks 8 and 9 done

--- src/app.js
+++ src/app.js
@@ -31,12 +31,11 @@ app.use((req, res, next) => {
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/', require('./routes/createProject'));
 app.use('/users', require('./routes/users'));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     app.listen(3000, () => console.log('server started'));
-}
-
-);
+});
 
 
---
+++ src/routes/createProject.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const ModelProject = require('../models/project');
+const { ensureAuthenticated } = require('../config/authenticated');
+
+router.get('/createProject', ensureAuthenticated, (req, res)  => {
+    res.render('createProject');
+});
+
+router.post('/createProject', ensureAuthenticated, (req, res)  => {
+    const projectName = req.body.name;
+    const projectDescription = req.body.description;
+
+    let errors = [];
+
+    if (!projectName) {
+        errors.push({ msg: 'Vous devez donner un nom à votre projet' });
+    }
+
+    if (projectName.length > 40) {
+        errors.push({ msg: 'Le nom de votre projet doit être inférieur à 40 caractères' });
+    }
+
+    if (projectDescription && projectDescription.length > 300) {
+        errors.push({ msg: 'La description de votre projet doit prendre moins de 300 caractères' });
+    }
+
+    console.log(errors.values);
+    if (errors.length == 0) {
+        const newProject = new ModelProject({
+            name: projectName,
+            description: projectDescription,
+            users: [],
+            sprints: [],
+            releases: []
+        });
+
+        newProject.save();
+        res.redirect('/Projects');
+    }
+    else {
+        res.render('/createProject', {
+            errors,
+            projectName,
+            projectDescription
+        });
+    }
+});
+
+module.exports = router;
