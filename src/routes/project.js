faa0b1eb15a2ed0fbb50b2602e15570c765c6947
wassim <berrariwassim@gmail.com>
2019-11-10 20:37:04 +0100

    add user to project Done

--- src/routes/project.js
+++ src/routes/project.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ModelProject = require('../models/project');
+const ModelUser = require('../models/user');
 const { ensureAuthenticated } = require('../config/authenticated');
 
 // Page displaying the main information about the selected project
-router.get('/project/:projectId', ensureAuthenticated, (req, res)  => displayProjectPage(req, res));
+router.get('/project/:projectId', ensureAuthenticated, (req, res) => displayProjectPage(req, res));
 
-async function displayProjectPage(req, res)
-{
+async function displayProjectPage(req, res) {
     req.session.projectId = require('mongodb').ObjectID(req.params.projectId);
 
-    await ModelProject.findOne({_id: req.session.projectId})
+    await ModelProject.findOne({ _id: req.session.projectId })
         .then(
             project => {
                 req.session.projectName = project.name;
@@ -19,15 +19,15 @@ async function displayProjectPage(req, res)
         )
         .catch(err => console.log("Couldn't find this project: " + err));
 
-    res.render('project', { 
-        projectId : req.session.projectId,
-        projectName : req.session.projectName,
-        projectDesc : req.session.projectDesc 
+    res.render('project', {
+        projectId: req.session.projectId,
+        projectName: req.session.projectName,
+        projectDesc: req.session.projectDesc
     });
 }
 
 // Modification of the name or description of the selected project
-router.post('/project/:projectId', ensureAuthenticated, (req, res)  => {
+router.post('/project/:projectId', ensureAuthenticated, (req, res) => {
     const newProjectName = req.body.projectName;
     const newProjectDescription = req.body.projectDescription;
 
@@ -39,10 +39,10 @@ router.post('/project/:projectId', ensureAuthenticated, (req, res)  => {
         }
 
         else {
-            ModelProject.updateOne({_id: req.session.projectId}, {
+            ModelProject.updateOne({ _id: req.session.projectId }, {
                 name: newProjectName
-            }, function() {});
-        
+            }, function () { });
+
             req.session.projectName = newProjectName
         }
     }
@@ -52,10 +52,10 @@ router.post('/project/:projectId', ensureAuthenticated, (req, res)  => {
             errors.push({ msg: 'La description de votre projet doit prendre moins de 300 caractères' });
         }
         else {
-            ModelProject.updateOne({_id: req.session.projectId}, {
+            ModelProject.updateOne({ _id: req.session.projectId }, {
                 description: newProjectDescription
-            }, function() {});
-        
+            }, function () { });
+
             req.session.projectDesc = newProjectDescription
         }
     }
@@ -65,48 +65,95 @@ router.post('/project/:projectId', ensureAuthenticated, (req, res)  => {
     }
 
     if (errors.length == 0) {
-        res.render('project', { 
-            projectId : req.session.projectId,
-            projectName : req.session.projectName,
-            projectDesc : req.session.projectDesc 
+        res.render('project', {
+            projectId: req.session.projectId,
+            projectName: req.session.projectName,
+            projectDesc: req.session.projectDesc
         });
     }
     else {
         res.render('project', {
             errors,
-            projectId : req.session.projectId,
-            projectName : req.session.projectName,
-            projectDesc : req.session.projectDesc 
+            projectId: req.session.projectId,
+            projectName: req.session.projectName,
+            projectDesc: req.session.projectDesc
         });
     }
-    
+
 });
 
 // Click on the edit button next to the project name
-router.get('/project/:projectId/modifyName', ensureAuthenticated, (req, res)  => {
-    res.render('modifyProjectName', { 
-        projectId : req.session.projectId,
-        projectName : req.session.projectName,
-        projectDesc : req.session.projectDesc 
+router.get('/project/:projectId/modifyName', ensureAuthenticated, (req, res) => {
+    res.render('modifyProjectName', {
+        projectId: req.session.projectId,
+        projectName: req.session.projectName,
+        projectDesc: req.session.projectDesc
     });
 });
 
 // Click on the "modify" button
-router.get('/project/:projectId/modifyDescription', ensureAuthenticated, (req, res)  => {
-    res.render('modifyProjectDescription', { 
-        projectId : req.session.projectId,
-        projectName : req.session.projectName,
-        projectDesc : req.session.projectDesc 
+router.get('/project/:projectId/modifyDescription', ensureAuthenticated, (req, res) => {
+    res.render('modifyProjectDescription', {
+        projectId: req.session.projectId,
+        projectName: req.session.projectName,
+        projectDesc: req.session.projectDesc
     });
 });
 
-router.get('/project/:projectId/delete', ensureAuthenticated, (req, res)  => {
-    ModelProject.deleteOne({_id: req.session.projectId}, function() {});
-    
+router.get('/project/:projectId/delete', ensureAuthenticated, (req, res) => {
+    ModelProject.deleteOne({ _id: req.session.projectId }, function () { });
+
     res.render('Projects', {
         user: req.user,
-        
+
     });
+});
+router.get('/project/:projectId/addUser', ensureAuthenticated, (req, res) => {
+    res.render('addUser', {
+        projectId: req.params.projectId,
+        user: req.user
+    });
+});
+router.post('/project/:projectId/addUser', ensureAuthenticated, (req, res) => {
+    const projectId = req.body.projectId;
+    const newUser = req.body.newUser;
+    const errors = [];
+
+    ModelUser.findOne({ email: newUser })
+        .then(user => {
+            if (user) {
+                //User is registred 
+                var userToadd = { email: newUser };
+                ModelProject.find({ _id: projectId, users: {$elemMatch: { email: newUser } }})
+                    .then(project => {
+                        //Checking if user already assigned to the project
+                        if (project.length > 0) {
+                            errors.push({ msg: 'l\'utilisateur est deja un collaborateurs du projet' });
+                        } else {
+                             ModelProject.updateOne({ _id: projectId },
+                                { $push: { users: userToadd } }, (succ, err) => {
+                                    if(err){
+                                        errors.push({ msg: 'Ajout non effectuer'+err });
+                                    }
+                                }
+                            );
+                        }
+                    }).catch(err => errors.push({ msg: err }));
+
+            } else {      
+                errors.push({ msg: 'l\'utilisateur n\'existe pas' });
+            }
+        }).catch(err => errors.push({ msg: err }));
+    if (errors.length > 0) {
+        res.render('/Projects', {
+            projectId: projectId,
+            errors: errors,
+            user: req.user
+        });
+    } else {
+        res.redirect('/Projects');
+    }
+
 });
 
 module.exports = router;
