24ca01e422308de55d21f71351ce088d2aaa4c02
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-11-18 14:18:56 +0100

    cud sprint handle errors

--- src/routes/sprint.js
+++ src/routes/sprint.js
@@ -14,86 +14,142 @@ router.get('/project/:projectId/createSprint', ensureAuthenticated, (req, res) =
 
 router.post('/project/:projectId/createSprint', ensureAuthenticated, (req, res) => {
     const errors = [];
-
     const projectId = req.params.projectId;
     const sprintName = req.body.name;
     const startDate = req.body.startDate;
     const endDate = req.body.endDate;
 
-    let sprint = { name: sprintName, startDate: new Date(startDate), endDate: new Date(endDate) };
+    if (!sprintName) {
+        errors.push({ msg: 'Vous devez donner un nom à votre sprint' });
+    }
 
-    ModelProject.updateOne({ _id: projectId },
-        { $push: { sprints: sprint } }, (succ, err) => {
-            if (err) {
-                errors.push({ msg: 'Erreur lors de l\'ajout du sprint: ' + err });
+    else if (sprintName.length > 40) {
+        errors.push({ msg: 'Le nom de votre sprint doit être inférieur à 40 caractères' });
+    }
+
+    if (!startDate) {
+        errors.push({ msg: 'Vous devez donner une date de début à votre sprint' });
+    }
+
+    if (!endDate) {
+        errors.push({ msg: 'Vous devez donner une date de fin à votre sprint' });
+    }
+
+    if (errors.length === 0) {
+        let sprint = { name: sprintName, startDate: new Date(startDate), endDate: new Date(endDate) };
+
+        ModelProject.updateOne({ _id: projectId },
+            { $push: { sprints: sprint } }, 
+            function(err, succ) {
+                if (err) {
+                    console.log("Erreur lors de la création du sprint: " + err);
+                }
+                else {
+                    ModelProject.findOne({ _id: projectId }).then(project => {
+                        ModelUserStory.find({ projectId: projectId })
+                            .then(userStorys => {
+                                res.render('project', {
+                                    project: project,
+                                    moment: moment,
+                                    orphanUs: userStorys
+                                });
+                            })
+                            .catch(err => console.log("Couldn't find this project: " + err));
+                    });
+                }
             }
-        }
-    );
-
-    if (errors.length > 0) {
-        res.render('/project/:projectId/createSprint', {
-            projectId: projectId,
-            errors: errors,
-            user: req.user
-        });
+        );
     }
     else {
-        res.redirect('/project/' + req.params.projectId);
+        res.render('createSprint', {
+            errors: errors,
+            projectId: projectId
+        });
     }
-
 });
 
-async function getModifySprint(req, res) {
+router.get('/project/:projectId/modifySprint/:sprintId', ensureAuthenticated, (req, res) => {
     const projectId = req.params.projectId;
     const sprintId = req.params.sprintId;
 
-    let sprintToUpdate;
-
-    await ModelProject.findOne(
+    ModelProject.findOne(
         { 'sprints._id' : sprintId }, 
         { 'sprints.$': 1 }, 
         function(err, project) {
-            sprintToUpdate = project.sprints[0];
-         }
-    );
-
-    res.render('modifySprint', {
-        projectId: projectId,
-        sprintId: sprintId,
-        sprint: sprintToUpdate,
-        moment: moment
-    });
-}
-
-router.get('/project/:projectId/modifySprint/:sprintId', ensureAuthenticated, (req, res) => getModifySprint(req, res));
+            if (err) {
+                console.log("Couldn't find the sprint: "+ err)
+            }
+            else {
+                res.render('modifySprint', {
+                    projectId: projectId,
+                    sprintId: sprintId,
+                    sprintName: project.sprints[0].name,
+                    sprintStartDate: project.sprints[0].startDate,
+                    sprintEndDate: project.sprints[0].endDate,
+                    moment: moment
+                });
+            }
+        });
+});
 
 router.post('/project/:projectId/modifySprint/:sprintId', ensureAuthenticated, (req, res) => {
+    const errors = [];
     const projectId = req.params.projectId;
     const sprintId = req.params.sprintId;
     const newSprintName = req.body.name;
     const newSprintStartDate = req.body.startDate;
     const newSprintEndDate = req.body.endDate;
 
-    ModelProject.updateOne(
-        { 'sprints._id' : sprintId },
-        { "$set": { "sprints.$.name": newSprintName , "sprints.$.startDate": newSprintStartDate, "sprints.$.endDate":newSprintEndDate } },
-        function(err) {
-            if (err) {
-                console.log("Could not update this sprint: " + err);
-            }
-        }
-    );
+    if (!newSprintName) {
+        errors.push({ msg: 'Vous devez donner un nom à votre sprint' });
+    }
 
-    ModelProject.findOne({ _id: projectId }).then(project => {
-        ModelUserStory.find({ projectId: projectId })
-            .then(userStorys => {
-                res.render('project', {
-                    project: project,
-                    moment: moment,
-                    orphanUs: userStorys
-                });
-            }).catch(err => console.log("Couldn't find this project: " + err));
-    })
+    else if (newSprintName.length > 40) {
+        errors.push({ msg: 'Le nom de votre sprint doit être inférieur à 40 caractères' });
+    }
+
+    if (!newSprintStartDate) {
+        errors.push({ msg: 'Vous devez donner une date de début à votre sprint' });
+    }
+
+    if (!newSprintEndDate) {
+        errors.push({ msg: 'Vous devez donner une date de fin à votre sprint' });
+    }
+
+    if (errors.length === 0) {
+        ModelProject.updateOne(
+            { 'sprints._id' : sprintId },
+            { "$set": { "sprints.$.name": newSprintName , "sprints.$.startDate": newSprintStartDate, "sprints.$.endDate":newSprintEndDate } },
+            function(err) {
+                if (err) {
+                    console.log("Couldn't update the sprint: "+ err)
+                }
+                else {
+                    ModelProject.findOne({ _id: projectId }).then(project => {
+                        ModelUserStory.find({ projectId: projectId })
+                            .then(userStorys => {
+                                res.render('project', {
+                                    project: project,
+                                    moment: moment,
+                                    orphanUs: userStorys
+                                });
+                            }).catch(err => console.log("Couldn't find this project: " + err));
+                    });
+                }
+            }
+        );
+    }
+    else {
+        res.render('modifySprint', {
+            errors: errors,
+            projectId: projectId,
+            sprintId: sprintId,
+            sprintName: newSprintName,
+            sprintStartDate: new Date(newSprintStartDate),
+            sprintEndDate: new Date(newSprintEndDate),
+            moment: moment
+        });
+    }
 });
 
 router.get('/project/:projectId/deleteSprint/:sprintId', ensureAuthenticated, (req, res) => {
@@ -103,23 +159,24 @@ router.get('/project/:projectId/deleteSprint/:sprintId', ensureAuthenticated, (r
     ModelProject.updateOne(
         { _id: projectId },
         { "$pull": { sprints: { _id : sprintId } } },
-        function(err) {
+        function(err, succ) {
             if (err) {
                 console.log("Could not delete this sprint: " + err);
             }
+            else {
+                ModelProject.findOne({ _id: projectId }).then(project => {
+                    ModelUserStory.find({ projectId: projectId })
+                        .then(userStorys => {
+                            res.render('project', {
+                                project: project,
+                                moment: moment,
+                                orphanUs: userStorys
+                            });
+                        }).catch(err => console.log("Couldn't find this project: " + err));
+                });
+            }
         }
     );
-
-    ModelProject.findOne({ _id: projectId }).then(project => {
-        ModelUserStory.find({ projectId: projectId })
-            .then(userStorys => {
-                res.render('project', {
-                    project: project,
-                    moment: moment,
-                    orphanUs: userStorys
-                });
-            }).catch(err => console.log("Couldn't find this project: " + err));
-    })
 });
 
 module.exports = router;
