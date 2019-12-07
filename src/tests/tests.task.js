032aa8a1f6e0e402c78e9cec8e4b6cd4764a5ba5
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-12-07 15:23:34 +0100

    fix link task test

--- src/tests/tests.task.js
+++ src/tests/tests.task.js
@@ -27,7 +27,6 @@ describe('controller.task', function() {
         });
         const projectId = input.projectId;
         const taskId = input.taskId;
-        const userStoryId = input.userStoryId;
         it('/GET /project/projectId + tasks routes', function(done) {
             request.get('/project/' + projectId)
             .set('Cookie', mochaSession)
@@ -36,7 +35,6 @@ describe('controller.task', function() {
                 expect(res.text).to.nested.include('/project/'+projectId+'/createTask');
                 expect(res.text).to.nested.include('/modifyTask/'+taskId);
                 expect(res.text).to.nested.include('/deleteTask/'+taskId);
-                expect(res.text).to.nested.include('/linkTask/'+taskId);
                 done();
             });
         });
@@ -48,15 +46,6 @@ describe('controller.task', function() {
                 expect(res.text).to.nested.include('<option>Mocha@auto.com</option>');
                 expect(res.text).to.nested.include('min=\"1\" max=\"3\" value=\"1\"');
                 expect(res.text).to.nested.include('première tâche</textarea>');
-                done();
-            });
-        });
-        it('/GET /project/projectId/linkTask/taskId', function(done) {
-            request.get('/project/'+projectId+'/linkTask/'+taskId)
-            .set('Cookie', mochaSession)
-            .end((err, res) => {
-                expect(res.text).to.nested.include('first orphan us of mocha');
-                expect(res.text).to.nested.include(userStoryId);
                 done();
             });
         });
