5816da1f9aae44e12d342d26e23524af367dca77
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-12-09 23:13:14 +0100

    delete releases

--- src/routes/release.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controllerRelease = require('../controller/controller.release');
-const { ensureAuthenticated } = require('../config/authenticated');
-
-router.get('/release/:projectId', ensureAuthenticated, controllerRelease.displayRelease);
-module.exports = router;
